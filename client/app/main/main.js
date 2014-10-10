f899783aead3bb4c4999fdea96eab66bdeead03f
Rajat Khanna <rajatkhanna82@gmail.com>
2014-10-10 00:05:30 -0700

    first commit

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('reputationApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
